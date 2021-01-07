48daeb07286cd7b28a95fb5cf78d5b82ea297d76
abhishek-pes <abhishekmis402gmail.com>
2021-01-07 18:36:30 +0530

    added posts route

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(cors());
 //routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/login", require("./routes/api/login"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("server started on port " + PORT));
---
+++ frontend/src/context/auth-context.js
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+export const authcontext = createContext({
+  isLoggedin: true,
+  login: () => {},
+  logout: () => {},
+});
